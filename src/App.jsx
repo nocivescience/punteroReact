110ad2116a627a8e21f9dcc4b70b2997c5bf8a2c
ricardo <nocive.science@gmail.com>
2024-08-26 04:35:17 -0400

    los cuadros están deslocalizados

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import { useState } from 'react'
+
 import './App.css'
 import Contenedor from './componentes/Contenedor'
 import Navbar from '../src/componentes/Navbar'
+import Canvas from '../src/componentes/Canvas'
+import {BrowserRouter as Router, Route, Routes, BrowserRouter} from 'react-router-dom'
+
 
 function App() {
     return (
-        <div className="App">
+        <BrowserRouter basename='/punteroReact'>
             <Navbar />
-            <h1>App</h1>
-            <Contenedor />
-        </div>
+            <Routes>
+                <Route path="/" element={<Contenedor />} />
+                <Route path="/canvas" element={<Canvas />} />
+            </Routes>
+        </BrowserRouter>
     )
 }
 
---
+++ src/componentes/Canvas.jsx
@@ -0,0 +1,8 @@
+import { useState, useEffect, useRef , React} from "react";
+const Canvas = () => {
+    return (
+        <canvas id="canvas" width="500" height="500"></canvas>
+    )
+}
+
+export default Canvas
